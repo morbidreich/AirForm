55c39a80b71041b54af4b6af07fa8f959eca484a
Bartek Kujda <bartek.kujda@gmail.com>
2021-12-07 20:42:35 +0100

    initial

---
+++ src/main/java/io/github/morbidreich/airform/AirFormApplication.java
@@ -0,0 +1,13 @@
+package io.github.morbidreich.airform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/morbidreich/airform/AirFormApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.morbidreich.airform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
