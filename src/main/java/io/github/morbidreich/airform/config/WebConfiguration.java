369a28b323cff8de2ea3c3b7f6a70e86e7c405fb
Bartek Kujda <bartek.kujda@gmail.com>
2021-12-19 11:00:11 +0100

    html cosmetics

--- src/main/java/io/github/morbidreich/airform/config/WebConfiguration.java
+++ src/main/java/io/github/morbidreich/airform/config/WebConfiguration.java
@@ -1,8 +1,11 @@
 package io.github.morbidreich.airform.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
