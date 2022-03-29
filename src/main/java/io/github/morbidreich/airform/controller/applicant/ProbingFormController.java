6871bbd2e9acf8c736e17761102cf4d7441d91c7
Bartek Kujda <bartek.kujda@gmail.com>
2022-03-29 17:04:03 +0200

    added @ModelAttribute for probing form creation

--- src/main/java/io/github/morbidreich/airform/controller/applicant/ProbingFormController.java
+++ src/main/java/io/github/morbidreich/airform/controller/applicant/ProbingFormController.java
@@ -3,6 +3,9 @@ package io.github.morbidreich.airform.controller.applicant;
 import io.github.morbidreich.airform.entity.forms.ProbingForm;
 import io.github.morbidreich.airform.service.BaseFormService;
 import io.github.morbidreich.airform.service.ProbingFormService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,6 +23,7 @@ public class ProbingFormController {
 
     private final BaseFormService baseFormService;
     private final ProbingFormService probingFormService;
+    Logger log = LoggerFactory.getLogger(ProbingFormController.class);
 
     public ProbingFormController(BaseFormService baseFormService,
                                  ProbingFormService probingFormService) {
@@ -28,12 +32,15 @@ public class ProbingFormController {
     }
 
     @GetMapping("/probing-form")
-    public String getProbingForm(Model model, Principal principal) {
-        ProbingForm probingForm = new ProbingForm();
-        probingForm = (ProbingForm) baseFormService.prepopulateForm(probingForm, principal.getName());
-        model.addAttribute("form", probingForm);
-
+    public String getProbingForm() {
         return "asm-forms/stratospheric-baloon-form";
+    }
+
+    @ModelAttribute(name = "form")
+    public ProbingForm getProbingForm(Principal principal) {
+        log.info("created new ProbingForm via @ModelAttr annotated method");
+
+        return (ProbingForm) baseFormService.prepopulateForm(new ProbingForm(), principal.getName());
     }
 
     @PostMapping("/probing-form-save")
